b0272e5ef5013d9d4ad6781d036d167d7d75b250
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-02 17:11:15 +0000

    Add README.md for public repo. Add template to render when using isPending property of useQuery hook and simluate slow fetch call to enable it to be percievable. WIP.

--- tanstack-query-practice/src/App.tsx
+++ tanstack-query-practice/src/App.tsx
@@ -2,23 +2,30 @@ import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
-  /* useQuery imported from TanstackQuery, take in 1 primary argument:
+  /* useQuery hook is imported from TanstackQuery, takes in 1 primary argument:
   an object which needs 2 properties:
-  A query key used for refectching and caching data, must be unique 
-  A query function denoted as queryFn: will run whenevr run the query with this key, such as your API call (fetch, axios, etc)
-
+  A query key used for refetching and caching data, must be unique 
+  A query function denoted as queryFn: will run whenever run the query with this 
+  key, such as your API call (fetch, axios, etc)
   */
 
-  const { data } = useQuery({
+  const { data, isPending } = useQuery({
     queryKey: ["todos"],
-    // Do not call the queryFn here just pass it
+    // queryFn is not called by this hook, just pass it
     queryFn: getTodos,
   });
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      {isPending ? <span>Loading...</span> : JSON.stringify(data.slice(0, 10))}
+    </div>
+  );
 }
 
 const getTodos = async () => {
+  /* Simulate a slow fetch and be able to view template 
+  rendered when isPending is true */
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch("https://jsonplaceholder.typicode.com/todos");
   return await response.json();
 };
