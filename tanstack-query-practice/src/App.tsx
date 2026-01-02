059880028477b518591fc1044457bde302996a8a
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-02 15:19:07 +0000

    Add useQuery hook with queryKey and function to get data. Print to the browser.

--- tanstack-query-practice/src/App.tsx
+++ tanstack-query-practice/src/App.tsx
@@ -1,7 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
-  return <></>;
+  /* useQuery imported from TanstackQuery, take in 1 primary argument:
+  an object which needs 2 properties:
+  A query key used for refectching and caching data, must be unique 
+  A query function denoted as queryFn: will run whenevr run the query with this key, such as your API call (fetch, axios, etc)
+
+  */
+
+  const { data } = useQuery({
+    queryKey: ["todos"],
+    // Do not call the queryFn here just pass it
+    queryFn: getTodos,
+  });
+
+  return <div>{JSON.stringify(data)}</div>;
 }
 
+const getTodos = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  return await response.json();
+};
 export default App;
