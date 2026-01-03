c33f60c87760a251e47027bb705ca9f2dd36f11b
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-01-03 16:12:27 +0000

    Delete redundant example lines. Extract the queryOptions into new function createTodoQueryOptions in separate ts file, export as default and use in App.tsx as the queryOptions. Place function getTodos below for convenience in this example, typically would be in a separate file.

---
+++ tanstack-query-practice/src/queryOptions/createTodoQueryOptions.ts
@@ -0,0 +1,23 @@
+import { queryOptions } from "@tanstack/react-query";
+
+/* Utility allows for type saftey and intellisense
+ auto-complete options versus manually typing out objects */
+
+export default function createTodoQueryOptions() {
+  return queryOptions({
+    // query will be cached using it's queryKey, needs id for accuracy if values will change
+    queryKey: ["todos" /*id*/],
+    // queryFn is not called by this hook, pass in function definition not a call
+    queryFn: /*() =>*/ getTodos /*(id)*/,
+  });
+}
+
+const getTodos = async (/*id: number*/) => {
+  /* Simulate a slow fetch and be able to view template 
+  rendered when isPending is true */
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${1}`
+  );
+  return await response.json();
+};
