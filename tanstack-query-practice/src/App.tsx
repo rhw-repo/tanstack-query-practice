ffc02da168c8f52f67fd560b141161fcefaa4099
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-01-03 15:11:18 +0000

    Add state for id. Pass id to queryKey. Pass function definition (without calling it) for getTodos to the queryFn. Allows demo of how ids are essential tp pass paramaters to useQuery hook: without id, cannot keep track of query accurately

--- tanstack-query-practice/src/App.tsx
+++ tanstack-query-practice/src/App.tsx
@@ -1,20 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [id, setId] = useState(1);
+  const [on, setOn] = useState(true);
   /* useQuery hook is imported from TanstackQuery, takes in 1 primary argument:
   an object which needs 2 properties:
-  A query key used for refetching and caching data, must be unique 
+  A query key used for refetching and caching data, 
+  must be unique, needs id to be accurate if variable will change
   A query function denoted as queryFn: will run whenever run the query with this 
   key, such as your API call (fetch, axios, etc)
   */
 
-  const { data, isPending, refetch } = useQuery({
+  const { data, isPending, refetch, error } = useQuery({
     // query will be cached using it's queryKey
-    queryKey: ["todos"],
-    // queryFn is not called by this hook, just pass it
-    queryFn: getTodos,
+    queryKey: ["todos", id],
+    // queryFn is not called by this hook, pass in function definition using () not a call
+    queryFn: () => getTodos(id),
   });
+
+  if (error) {
+    alert("Something went wrong");
+  }
 
   return (
     <main>
@@ -25,17 +33,19 @@ function App() {
           JSON.stringify(data.slice(0, 10))
         )}
       </div>
-      // Already
       <button onClick={() => refetch()}>Refetch</button>
+      <button onClick={() => setId((prev) => prev + 1)}>Increment ID</button>
     </main>
   );
 }
 
-const getTodos = async () => {
+const getTodos = async (id: number) => {
   /* Simulate a slow fetch and be able to view template 
   rendered when isPending is true */
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${id}`
+  );
   return await response.json();
 };
 
