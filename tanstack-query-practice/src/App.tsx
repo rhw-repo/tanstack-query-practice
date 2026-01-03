8947ef1a3ce1047e5ba966bd698fcde535ce5dd2
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-03 16:12:27 +0000

    Delete redundant example lines. Extract the queryOptions into new function createTodoQueryOptions in separate ts file, export as default and use in App.tsx as the queryOptions. Place function getTodos below for convenience in this example, typically would be in a separate file.

--- tanstack-query-practice/src/App.tsx
+++ tanstack-query-practice/src/App.tsx
@@ -1,24 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import "./App.css";
-import { useState } from "react";
+// queryOptions extracted into separate ts file to be reusable
+import createTodoQueryOptions from "./queryOptions/createTodoQueryOptions";
 
 function App() {
-  const [id, setId] = useState(1);
-  const [on, setOn] = useState(true);
   /* useQuery hook is imported from TanstackQuery, takes in 1 primary argument:
   an object which needs 2 properties:
-  A query key used for refetching and caching data, 
+  - A query key used for refetching and caching data, 
   must be unique, needs id to be accurate if variable will change
-  A query function denoted as queryFn: will run whenever run the query with this 
+  - A query function denoted as queryFn: will run whenever run the query with this 
   key, such as your API call (fetch, axios, etc)
   */
 
-  const { data, isPending, refetch, error } = useQuery({
-    // query will be cached using it's queryKey
-    queryKey: ["todos", id],
-    // queryFn is not called by this hook, pass in function definition using () not a call
-    queryFn: () => getTodos(id),
-  });
+  const { data, isPending, refetch, error } = useQuery(
+    createTodoQueryOptions()
+  );
 
   if (error) {
     alert("Something went wrong");
@@ -34,20 +30,9 @@ function App() {
         )}
       </div>
       <button onClick={() => refetch()}>Refetch</button>
-      <button onClick={() => setId((prev) => prev + 1)}>Increment ID</button>
     </main>
   );
 }
-
-const getTodos = async (id: number) => {
-  /* Simulate a slow fetch and be able to view template 
-  rendered when isPending is true */
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${id}`
-  );
-  return await response.json();
-};
 
 export default App;
 
@@ -55,4 +40,19 @@ export default App;
 isFetching runs whenever the function is running at all 
 isPending runs if there's no cache data 
 isLoading runs when query is loading for the very first time 
+*/
+
+/* 
+To run conditional queries: 
+set state using a boolean
+pass property enabled set to on to the useQuery hook 
+whenever the boolean is true, the query will automatically run
+whenever it is false, the query will not automatically run
+
+It would run if forced to, for eaxmple if refetch function fires
+But will not automatically run each time React rerenders this component
+
+Use cases:
+If there are multiple useQuery hooks in your component but each should 
+only be run depending on some state in the component
 */
